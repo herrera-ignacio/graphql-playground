1a402e9e8391d72b8444d5ccffe76b2ae894e94f
Ignacio Herrera <ignacioromanherrera@gmail.com>
2021-12-07 23:10:15 -0300

    Mutations CRUD

--- node/mutations/README.md
+++ node/mutations/README.md
@@ -1,3 +1,29 @@
 # Relational Data
 
-![](.README_images/abc047fb.png)
+## Mutations
+
+### Create user
+
+```graphql
+mutation {
+  createUser(
+    userInput: {
+      name: "Nacho"
+      email: "nacho@example.com"
+      age: 26
+  	}
+  ){
+  	name
+  }
+}
+```
+
+### Create Post
+
+```graphql
+mutation {
+  createPost(title: "My post", body: "Test body", published: true, author: 123) {
+     _id
+  }
+}
+```
--- node/mutations/src/index.js
+++ node/mutations/src/index.js
@@ -1,9 +1,9 @@
 import { GraphQLServer } from 'graphql-yoga';
 import { v4 as uuidv4 } from 'uuid';
 
-const usersData = [...require('./users.json')];
-const postsData = [...require('./posts.json')];
-const commentsData = [...require('./comments.json')];
+let usersData = [...require('./users.json')];
+let postsData = [...require('./posts.json')];
+let commentsData = [...require('./comments.json')];
 
 const typeDefs = `
 	type Query {
@@ -14,7 +14,25 @@ const typeDefs = `
 	}
 	
 	type Mutation {
-		createUser(name: String!, email: String!, age: Int): User!
+		createUser(userInput: CreateUserInput): User!
+		createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+		createComment(text: String!, author: ID!, post: ID!): Comment!
+		deleteUser(_id: ID!): User!
+		deletePost(_id: ID!): Post!
+		deleteComment(id: ID!): Comment!
+		updateUser(_id: ID!, userInput: UpdateUserInput!): User!
+	}
+	
+	input CreateUserInput {
+		name: String!
+		email: String!
+		age: Int
+	}
+	
+	input UpdateUserInput {
+		name: String
+		email: String
+		age: Int
 	}
 	
 	type User {
@@ -98,21 +116,94 @@ const resolvers = {
 				throw new Error('Email already used');
 			}
 
-			const user = {
-				_id: uuidv4(),
-				name: args.name,
-				email: args.email,
-				age: args.age,
+			const user = { _id: uuidv4(), ...args };
+			usersData.push(user);
+			return user;
+		},
+
+		createPost(_, args) {
+			const userExists = usersData.some(user => user._id === args.author);
+
+			if (!userExists) {
+				throw new Error("Invalid user");
 			}
 
-			usersData.push(user);
+			const post = { id: uuidv4(), ...args }
+			postsData.push(post);
+			return post;
+		},
 
-			return user;
+		createComment(_, args) {
+			const userExists = usersData.some(user => user._id === args.author);
+			const postExists = postsData.some(post => post._id === args.author);
+
+			if (!userExists) throw new Error("Invalid user");
+			if (!postExists) throw new Error("Invalid post");
+
+			const comment = { _id: uuidv4(), ...args };
+			commentsData.push(comment);
+			return comment;
+		},
+
+		deleteUser(_, { _id }) {
+			const userIndex = usersData.findIndex(user => user._id === _id);
+
+			if (userIndex === -1) {
+				throw new Error('Invalid user');
+			}
+
+			// Cascade delete
+			postsData = postsData.filter(post => {
+				if (post.author === _id) {
+					commentsData = commentsData.filter(comment => comment.post !== post._id);
+					return false;
+				}
+				return true;
+			});
+
+			return usersData.splice(userIndex, 1);
+		},
+
+		deletePost(_, { _id }) {
+			const postIndex = postsData.findIndex(post => post._id === _id);
+
+			if (postIndex === -1) {
+				throw new Error('Invalid post');
+			}
+
+			// Cascade delete
+			commentsData = commentsData.filter(comment => comment.post !== _id);
+
+			return postsData.splice(postIndex, 1);
+		},
+
+		deleteComment(_, { _id }) {
+			const commentIndex = commentsData.findIndex(comment => comment._id === _id);
+			if (commentIndex === -1) throw new Error('Invalid comment');
+			return commentsData.splice(commentIndex, 1);
+		},
+
+		updateUser(_, { _id, userInput }) {
+			const user = usersData.find(user => user._id = _id);
+
+			if (!user) throw new Error('Invalid user');
+
+			if (typeof userInput.email === 'string') {
+				const emailTaken = usersData.some(user => user.email === userInput.email);
+				if (emailTaken) throw new Error('Email is already used');
+				user.email = userInput.email;
+			}
+
+			if (typeof userInput.name === 'string') {
+				user.name = userInput.name;
+			}
+
+			if (!isNaN(Number(userInput.age))) {
+				user.age = Number(userInput.age);
+			}
 		}
 	}
 }
-
-
 
 new GraphQLServer({ typeDefs, resolvers })
 	.start(() => { console.log('Server running in localhost:4000'); });
